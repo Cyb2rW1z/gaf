3e974ed64792ecdf4eaaa8c2be51f91c1fce4cd7
Jbr AKA user32.dll <126031099+Cyb2rW1z@users.noreply.github.com>
2023-10-16 11:06:44 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# GAF - Get All URLs and Resources
+
+GAF (Get All URLs and Resources) is a Python tool that enables you to analyze a web page by tracking and listing the resources (e.g., images, scripts, styles) found on the page and the domains from which they are loaded.
+
+## Usage
+1. Ensure you have Python installed on your system.
+
+2. Install the required Python libraries by running the following commands:
+
+   ```bash
+   pip install requests
+   pip install beautifulsoup4
+
+Run the script using the following command: 
+python3 gaf.py -u <URL> -heads "Header1=Value1,Header2=Value2"
+    Replace gaf.py with the name of the Python script.
+    Replace <URL> with the URL of the web page you want to analyze.
+    You can include custom headers as a comma-separated list in the format "Header=Value".
+```
+## Example
+
+
+```bash
+
+python gaf.py -u https://example.com -heads "User-Agent=my-user-agent,Accept-Language=en-US"
+
+```
---
+++ gaf.py
@@ -0,0 +1,48 @@
+import argparse
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+def track_resources(url, headers=None):
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            print(f"Request to {url} was successful!")
+
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            for tag in soup.find_all(['img', 'script', 'link']):
+                if tag.has_attr('src'):
+                    resource_url = tag['src']
+                elif tag.has_attr('href'):
+                    resource_url = tag['href']
+                else:
+                    continue
+
+                resource_domain = urlparse(resource_url).netloc
+
+                print(f"Resource URL: {resource_url}")
+                print(f"Resource Domain: {resource_domain}")
+                print("---")
+        else:
+            print(f"Request to {url} failed with status code {response.status_code}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Track resources from a URL with custom headers.")
+    parser.add_argument("-u", "--url", required=True, help="URL to track resources")
+    parser.add_argument("-heads", "--headers", help="Custom headers as a comma-separated list")
+
+    args = parser.parse_args()
+
+    url_to_track = args.url
+    custom_headers = args.headers
+
+    headers = {}
+    if custom_headers:
+        for header in custom_headers.split(','):
+            key, value = header.strip().split('=')
+            headers[key] = value
+
+    track_resources(url_to_track, headers)
